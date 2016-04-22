7df1451a64d50a83afd245b98a783bc20f3c9787
Peter Donner <peter@markup.at>
2016-04-22 09:29:28 +0200

    Add zsh config and documentation

---
+++ README.md
@@ -0,0 +1,8 @@
+# My development environment as Docker Container
+The code is heavily inspired by https://github.com/markmandel/wrapping-clojure-tooling-in-containers.
+
+## build.sh
+Builds the "buildhost" Docker Container image.
+
+## install.sh
+Installs a buildhost script in /usr/local/bin.
--- container/startup.sh
+++ container/startup.sh
@@ -1,5 +1,5 @@
 groupadd --gid $HOST_GID $HOST_USER
-useradd $HOST_USER --home /home/$HOST_USER --gid $HOST_GID --uid $HOST_UID --shell /bin/bash
+useradd $HOST_USER --home /home/$HOST_USER --gid $HOST_GID --uid $HOST_UID --shell /usr/bin/zsh
 echo "$HOST_USER:pw" | chpasswd
 
 #setup lein for this user
@@ -9,6 +9,7 @@ cp -r /root/.lein/* /home/$HOST_USER/.lein/
 mkdir /home/$HOST_USER/.emacs.d
 cp -r /root/.emacs.d/* /home/$HOST_USER/.emacs.d/
 cp /root/.emacs /home/$HOST_USER/.emacs
+cat /etc/zsh/zshrc > /home/$HOST_USER/.zshrc
 
 #make sure all permissions are good to go.
 chown -R $HOST_USER:$HOST_USER /home/$HOST_USER
